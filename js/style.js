28ee2f66cc3331e6afac55c6eb9019dda646691e
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2023-12-11 09:53:15 +0500

    product_task&

---
+++ js/style.js
@@ -0,0 +1,21 @@
+window.addEventListener("scroll", function () {
+  toggleBacktop();
+});
+
+let backtop = document.getElementById("backtop");
+
+function toggleBacktop() {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 200
+  ) {
+    backtop.style.bottom = "20px";
+  } else {
+    backtop.style.bottom = "-50px";
+  }
+}
+
+function myFunction() {
+  let element = document.body;
+  element.classList.toggle("dark-mode");
+}
