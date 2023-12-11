dbc31dc116dcd8b01d0fa7f61707d067b7f862d6
Ahmadxon08 <akhmadxonmoydinov@gmail.com>
2023-12-11 18:31:44 +0500

    slide function is added

--- js/style.js
+++ js/style.js
@@ -15,7 +15,14 @@ function toggleBacktop() {
   }
 }
 
-function myFunction() {
-  let element = document.body;
-  element.classList.toggle("dark-mode");
-}
+let modeBtn = document.getElementById("dark-light");
+let main2 = document.querySelector(".main2");
+let main3 = document.querySelector(".main3");
+let main4 = document.querySelector(".main4");
+
+modeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("dark");
+  main2.classList.toggle("dark");
+  main3.classList.toggle("dark");
+  main4.classList.toggle("dark");
+});
